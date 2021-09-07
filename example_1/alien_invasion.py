00125e2b01c965322a3d11daff3248eca6a08690
bartosian <bartosian1989@gmail.com>
2021-09-06 21:24:10 -0400

    Updating objects to move on events

--- example_1/alien_invasion.py
+++ example_1/alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 
 class AlienInvasion:
@@ -17,6 +18,8 @@ class AlienInvasion:
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
+
         # Set the background color
         self.bg_color = (230, 230, 230)
 
@@ -24,16 +27,26 @@ class AlienInvasion:
         """Start the main loop for the game."""
 
         while True:
-            # Watch for keyboard and mouse events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
-            # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color)
+            self._check_events()
+            self._update_screen()
 
             # make the most recently drawn screen visible.
             pygame.display.flip()
+
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # Move the ship to the right.
+                    self.ship.rect.x += 1
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
 
 
 if __name__ == '__main__':
