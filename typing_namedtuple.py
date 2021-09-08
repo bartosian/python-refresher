01d9fc373fa5fe74e40dce14fea1ad48720bee9a
bartosian <bartosian1989@gmail.com>
2021-09-08 18:57:26 -0400

    Added data classes examples

---
+++ typing_namedtuple.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+from typing import NamedTuple
+
+
+class Coordinate(NamedTuple):
+    lat: float
+    lon: float
+
+    def __str__(self):
+        ns = 'N' if self.lat >= 0 else 'S'
+        we = 'E' if self.lon >= 0 else 'W'
+
+        return f'{abs(self.lat):.1f}°{ns}, {abs(self.lon):.1f}°{we}'
+
+
+@dataclass(frozen=True)
+class Coordinate:
+    lat: float
+    lon: float
+
+    def __str__(self):
+        ns = 'N' if self.lat >= 0 else 'S'
+        we = 'E' if self.lon >= 0 else 'W'
+        return f'{abs(self.lat):.1f}°{ns}, {abs(self.lon):.1f}°{we}'
