21c663dbe78531ee4b745b6178f41869ab8cf1bc
bartosian <bartosian1989@gmail.com>
2021-09-23 20:05:40 -0400

    Added data classes with descriptors examples

--- typing_namedtuple.py
+++ typing_namedtuple.py
@@ -1,8 +1,10 @@
 from dataclasses import dataclass, field
-from typing import NamedTuple
+from typing import NamedTuple, ClassVar, Optional
+from enum import Enum, auto
+from datetime import date
 
 
-class Coordinate(NamedTuple):
+class CoordinateA(NamedTuple):
     lat: float
     lon: float
 
@@ -14,7 +16,7 @@ class Coordinate(NamedTuple):
 
 
 @dataclass(frozen=True)
-class Coordinate:
+class CoordinateB:
     lat: float
     lon: float
 
@@ -24,7 +26,7 @@ class Coordinate:
         return f'{abs(self.lat):.1f}°{ns}, {abs(self.lon):.1f}°{we}'
 
 
-class Coordinate(NamedTuple):
+class CoordinateC(NamedTuple):
     lat: float
     lon: float
     reference: str = 'WGS84'
@@ -39,7 +41,6 @@ class DemoPlainClass:
 class DemoNTClass(NamedTuple):
     a: int
     b: float = 1.1
-    c = 'spam'
 
 
 @dataclass()
@@ -49,7 +50,73 @@ class DemoDataClass():
     c = 'spam'
 
 
+class Coordinate(NamedTuple):
+    """
+    Provides location infromation.
+    """
+
+    lat: float
+    lon: float
+
+    def __str__(self):
+        ns = 'N' if self.lat >= 0 else 'S'
+        we = 'E' if self.lon >= 0 else 'W'
+
+        return f'{abs(self.lat):.1f}°{ns}, {abs(self.lon):.1f}°{we}'
+
+
 @dataclass
 class ClubMember:
     name: str
     guests: list[str] = field(default_factory=list)
+
+
+@dataclass
+class HackerClubMember(ClubMember):
+    all_handles: ClassVar[set[str]] = set()
+    handle: str = ''
+
+    def __post_init__(self):
+        cls = self.__class__
+
+        if self.handle == '':
+            self.handle = self.name.split()[0]
+        if self.handle in cls.all_handles:
+            msg = f'handle {self.handle!r} already exists.'
+
+            raise ValueError(msg)
+
+        cls.all_handles.add(self.handle)
+
+
+class ResourceType(Enum):
+    BOOK = auto()
+    EBOOK = auto()
+    VIDEO = auto()
+
+
+@dataclass
+class Resource:
+    """Media resource description."""
+
+    identifier: str
+    title: str = '<undefined>'
+    creators: list[str] = field(default_factory=list)
+    date: Optional[date] = None
+    type: ResourceType = ResourceType.BOOK
+    description: str = ''
+    language: str = ''
+    subjects: list[str] = field(default_factory=list)
+
+    def __repr__(self):
+        cls = self.__class__
+        cls_name = cls.__name__
+        indent = ' ' * 4
+        res = [f'{cls_name}(']
+
+        for f in fields(cls):
+            value = getattr(self, f.name)
+            res.append(f'{indent}{f.name} = {value!r},')
+
+        res.append(')')
+        return '\n'.join(res)
