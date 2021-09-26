2a49c6a3847491e3870aa39f6045cdae0c29daeb
bartosian <bartosian1989@gmail.com>
2021-09-26 15:54:37 -0400

    Added partial usage examples

---
+++ callable.py
@@ -0,0 +1,45 @@
+import random
+
+
+class BingoCage:
+
+    def __init__(self, items):
+        self._items = list(items)
+        random.shuffle(self._items)
+
+    def pick(self):
+        try:
+            return self._items.pop()
+        except IndexError:
+            raise LookupError("pick from empty BingoCage")
+
+    def __call__(self):
+        return self.pick()
+
+
+def tag(name, *content, class_=None, **attrs):
+    """Generate one or more HTML tags"""
+
+    if class_ is not None:
+        attrs['class'] = class_
+
+    attr_pairs = (f' {attr}="{value}"' for attr, value
+                  in sorted(attrs.items()))
+
+    attr_str = ''.join(attr_pairs)
+
+    if content:
+        elements = (f'<{name}{attr_str}>{c}</{name}>'
+                    for c in content)
+        return '\n'.join(elements)
+    else:
+        return f'<{name}{attr_str} />'
+
+
+metro_data = [
+    ('Tokyo', 'JP', 36.933, (35.689722, 139.691667)),
+    ('Delhi NCR', 'IN', 21.935, (28.613889, 77.208889)),
+    ('Mexico City', 'MX', 20.142, (19.433333, -99.133333)),
+    ('New York-Newark', 'US', 20.104, (40.808611, -74.020386)),
+    ('São Paulo', 'BR', 19.649, (-23.547778, -46.635833)),
+]
---
+++ partial.py
@@ -0,0 +1,26 @@
+from urlib.request import urlopen
+
+
+class UrlTemplate:
+    def __init__(self, template):
+        self.template = template
+
+    def open(self, **kwargs):
+        return urlopen(self.template.foemat_mat(kwargs))
+
+
+yahoo = UrlTemplate(
+    'http://finance.yahoo.com/d/quotes.csv?s={names}&f={fields}')
+for line in yahoo.open(names='IBM,AAPL,FB', fields='sl1c1v'):
+    print(line.decode('utf-8'))
+
+
+def urltemplate(template):
+    def opener(**kwargs):
+        return urlopen(template.format_map(kwargs))
+
+    return opener
+
+
+yahoo = urltemplate(
+    'http://finance.yahoo.com/d/quotes.csv?s={names}&f={fields}')
