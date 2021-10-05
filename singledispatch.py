312c81ef6b5a888d7d21317a0117b4eb65872223
bartosian <bartosian1989@gmail.com>
2021-10-05 11:51:40 -0400

    Added singledispatch examples

---
+++ singledispatch.py
@@ -0,0 +1,30 @@
+from functools import singledispatch
+from collections import abc
+import fractions
+import decimal
+import html
+import numbers
+
+
+@singledispatch
+def htmlize(obj: object) -> str:
+    content = html.escape(repr(obj))
+    return f"<pre>{content}</pre>"
+
+
+@htmlize.register
+def _(text: str) -> str:
+    content = html.escape(text).replace("\n", "<br/>\n")
+    return f"<p>{content}</p>"
+
+
+@htmlize.register
+def _(seq: abc.Sequence) -> str:
+    inner = "</li>\n<li>".join(htmlize(item) for item in seq)
+    return "<ul>\n<li>" + inner + "</li>\n</ul>"
+
+
+@htmlize.register(fractions.Fraction)
+def _(x) -> str:
+    frac = fractions.Fraction(x)
+    return f"<pre>{frac.numerator}/{frac.denominator}</pre>"
