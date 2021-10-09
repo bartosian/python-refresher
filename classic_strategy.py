c64b7624de2729c3f807c28adf4772beb428c32f
bartosian <bartosian1989@gmail.com>
2021-10-09 11:22:37 -0400

    Refactored classic pattern to use higher order functions approach

--- classic_strategy.py
+++ classic_strategy.py
@@ -1,7 +1,7 @@
-from abc import ABC, abstractmethod
 from collections.abc import Sequence
+from dataclasses import dataclass
 from decimal import Decimal
-from typing import NamedTuple, Optional
+from typing import NamedTuple, Optional, Callable
 
 
 class Customer(NamedTuple):
@@ -18,10 +18,11 @@ class LineItem(NamedTuple):
         return self.price * self.quantity
 
 
-class Order(NamedTuple):
+@dataclass(frozen=True)
+class Order:
     customer: Customer
     cart: Sequence[LineItem]
-    promotion: Optional["Promotion"] = None
+    promotion: Optional[Callable[["Order"], Decimal]] = None
 
     def total(self) -> Decimal:
         totals = (item.total() for item in self.cart)
@@ -31,44 +32,30 @@ class Order(NamedTuple):
         if self.promotion is None:
             discount = Decimal(0)
         else:
-            discount = self.promotion.discount(self)
+            discount = self.promotion(self)
         return self.total() - discount
 
     def __repr__(self):
         return f"<Order total: {self.total():.2f} due: {self.due():.2f}>"
 
 
-class Promotion(ABC):
-    def discount(self, order: Order) -> Decimal:
-        """Return discount as a positive dollar amount"""
+def fidelity_promo(order: Order) -> Decimal:
+    rate = Decimal("0.05")
+    if order.customer.fidelity >= 1000:
+        return order.total() * rate
+    return Decimal(0)
 
 
-class FidelityPromo(Promotion):
-    """5% discount for customers with 1000 or more fidelity points"""
-
-    def discount(self, order: Order) -> Decimal:
-        rate = Decimal("0.05")
-        if order.customer.fidelity >= 1000:
-            return order.total() * rate
-        return Decimal(0)
+def bulk_item_promo(order: Order) -> Decimal:
+    discount = Decimal(0)
+    for item in order.cart:
+        if item.quantity >= 20:
+            discount += item.total() * Decimal("0.1")
+    return discount
 
 
-class BulkItemPromo(Promotion):
-    """10% discount fro each LineItem with 20 or more units"""
-
-    def discount(self, order: Order) -> Decimal:
-        discount = Decimal(0)
-        for item in order.cart:
-            if item.quantity >= 20:
-                discount += item.total() * Decimal("0.1")
-        return discount
-
-
-class LargeOrderPromo(Promotion):
-    """7% discount for orders with 10 or more distinct items"""
-
-    def discount(self, order: Order) -> Decimal:
-        distinct_items = {item.product for item in order.cart}
-        if len(distinct_items) >= 10:
-            return order.total() * Decimal("0.07")
-        return Decimal
+def large_order_promo(order: Order) -> Decimal:
+    distinct_items = {item.product for item in order.cart}
+    if len(distinct_items) >= 10:
+        return order.total() * Decimal("0.07")
+    return Decimal
